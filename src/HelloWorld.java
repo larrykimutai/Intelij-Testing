1827c680cc80db52ab7f0fb2fcbacad5bad22b55
Larry <larrykimutai@gmail.com>
2021-08-26 01:28:37 -0400

    n/a

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
-  public static void main(String args[]){
+  public static void main(String[] args){
     System.out.println("Hello World");
+    System.out.println("testing if this works on computer");
   }
 }
